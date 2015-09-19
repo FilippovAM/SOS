2189960871fb95ac088f1401e1d7d2ac0eae7c1d
FilippovAM <cyberross@rambler.ru>
2015-09-19 05:15:02 +0300

    first commit

---
+++ js/calling.js
@@ -0,0 +1,11 @@
+        $(window).load(function() {
+            $('.flexslider').flexslider({
+                animation: "slide",
+                animationLoop: false,
+                slideshow: false,
+                move: 1,
+                prevText: "",
+                nextText: "",
+                controlNav: true
+            });
+        });
